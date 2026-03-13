01cd5cf7afea7dbac034ac24936acc797713edd3
Matthias Harzer <matthias.harzer03@gmail.com>
2026-03-13 20:42:01 +0100

    feat: add more commands

--- knowledge/resource.go
+++ knowledge/resource.go
@@ -15,14 +15,14 @@ type resourceItem interface {
 	Text() string
 }
 
-type keyBind struct {
-	keys        string
+type actionPair struct {
+	action      string
 	description string
 }
 
-func (k keyBind) Text() string {
+func (k actionPair) Text() string {
 	key := color.New(color.FgMagenta).Add(color.Bold)
-	return fmt.Sprintf("%s – %s", key.Sprint(k.keys), k.description)
+	return fmt.Sprintf("%s – %s", key.Sprint(k.action), k.description)
 }
 
 type descriptiveResource struct {
@@ -52,79 +52,4 @@ func (nr nestedResource) Content() string {
 		lines = append(lines, item.Content())
 	}
 	return strings.Join(lines, "\n\n")
-}
-
-func LoadResources() map[string]Resource {
-	return map[string]Resource{
-		"tmux": nestedResource{
-			items: []Resource{
-				descriptiveResource{
-					title: "tmux key bindings – panes",
-					items: []resourceItem{
-						keyBind{
-							keys:        "Ctrl + b, \"",
-							description: "Split the current pane horizontally",
-						},
-						keyBind{
-							keys:        "Ctrl + b, %",
-							description: "Split the current pane vertically",
-						},
-						keyBind{
-							keys:        "Ctrl + b, o",
-							description: "Navigate to the next pane",
-						},
-						keyBind{
-							keys:        "Ctrl + b, ;",
-							description: "Close the current pane",
-						},
-						keyBind{
-							keys:        "Ctrl + b, z",
-							description: "Toggle zoom for the current pane",
-						},
-						keyBind{
-							keys:        "Ctrl + b, x",
-							description: "Kill the current pane",
-						},
-						keyBind{
-							keys:        "Ctrl + b, Space",
-							description: "Toggle between the last two panes",
-						},
-					},
-				},
-				descriptiveResource{
-					title: "tmux key bindings – windows",
-					items: []resourceItem{
-						keyBind{
-							keys:        "Ctrl + b, c",
-							description: "Create a new windows",
-						},
-						keyBind{
-							keys:        "Ctrl + b, d",
-							description: "Detach from session",
-						},
-						keyBind{
-							keys:        "Ctrl + b, w",
-							description: "List all tmux windows",
-						},
-						keyBind{
-							keys:        "Ctrl + b, &",
-							description: "Kill the current window",
-						},
-						keyBind{
-							keys:        "Ctrl + b, n",
-							description: "Navigate to the next window",
-						},
-						keyBind{
-							keys:        "Ctrl + b, p",
-							description: "Navigate to the previous window",
-						},
-						keyBind{
-							keys:        "Ctrl + b, ,",
-							description: "Rename the current window",
-						},
-					},
-				},
-			},
-		},
-	}
 }
---
+++ knowledge/resources.go
@@ -0,0 +1,110 @@
+package knowledge
+
+func LoadResources() map[string]Resource {
+	return map[string]Resource{
+		"tmux": nestedResource{
+			items: []Resource{
+				descriptiveResource{
+					title: "tmux key bindings – session management",
+					items: []resourceItem{
+						actionPair{
+							action:      "tmux new -s <session_name>",
+							description: "Create a new tmux session with the specified name",
+						},
+						actionPair{
+							action:      "tmux attach -t <session_name>",
+							description: "Attach to an existing tmux session",
+						},
+						actionPair{
+							action:      "tmux ls",
+							description: "List all tmux sessions",
+						},
+						actionPair{
+							action:      "tmux kill-session -t <session_name>",
+							description: "Kill the specified tmux session",
+						},
+					},
+				},
+				descriptiveResource{
+					title: "tmux key bindings – panes",
+					items: []resourceItem{
+						actionPair{
+							action:      "Ctrl + b, \"",
+							description: "Split the current pane horizontally",
+						},
+						actionPair{
+							action:      "Ctrl + b, %",
+							description: "Split the current pane vertically",
+						},
+						actionPair{
+							action:      "Ctrl + b, o",
+							description: "Navigate to the next pane",
+						},
+						actionPair{
+							action:      "Ctrl + b, ;",
+							description: "Close the current pane",
+						},
+						actionPair{
+							action:      "Ctrl + b, z",
+							description: "Toggle zoom for the current pane",
+						},
+						actionPair{
+							action:      "Ctrl + b, x",
+							description: "Kill the current pane",
+						},
+						actionPair{
+							action:      "Ctrl + b, Space",
+							description: "Toggle between the last two panes",
+						},
+					},
+				},
+				descriptiveResource{
+					title: "tmux key bindings – windows",
+					items: []resourceItem{
+						actionPair{
+							action:      "Ctrl + b, c",
+							description: "Create a new windows",
+						},
+						actionPair{
+							action:      "Ctrl + b, d",
+							description: "Detach from session",
+						},
+						actionPair{
+							action:      "Ctrl + b, w",
+							description: "List all tmux windows",
+						},
+						actionPair{
+							action:      "Ctrl + b, &",
+							description: "Kill the current window",
+						},
+						actionPair{
+							action:      "Ctrl + b, n",
+							description: "Navigate to the next window",
+						},
+						actionPair{
+							action:      "Ctrl + b, p",
+							description: "Navigate to the previous window",
+						},
+						actionPair{
+							action:      "Ctrl + b, ,",
+							description: "Rename the current window",
+						},
+					},
+				},
+				descriptiveResource{
+					title: "tmux key bindings – utility",
+					items: []resourceItem{
+						actionPair{
+							action:      "Ctrl + b, t",
+							description: "Show the current time",
+						},
+						actionPair{
+							action:      "Ctrl + b, ?",
+							description: "Show all key bindings",
+						},
+					},
+				},
+			},
+		},
+	}
+}
